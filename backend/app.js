696397bf75dac79ad439d1a720db99293500d684
shreyaspai77 <shreyaspai25@gmail.com>
2024-06-23 22:39:01 +0530

    made changes

--- backend/app.js
+++ backend/app.js
@@ -1,57 +1,104 @@
 const express = require("express");
 const cors = require("cors");
-// const bodyParser = require("body-parser");
 const fs = require("fs");
 const yaml = require("yaml");
+const { Pool } = require("pg");
+const { exec } = require("child_process");
+const path = require("path");
+const dotenv = require("dotenv");
+
+const envPath = path.join(__dirname, "..", ".env"); //change as per requirement
+dotenv.config({ path: envPath }); //.env config 
+const pool = new Pool({
+  user: process.env.user,
+  host: "localhost",
+  database: process.env.database,
+  password: process.env.password,
+  port: process.env.port,
+});
 
 const app = express();
-const port = 3000;
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+const port = 3001;
 
+const corsOptions = {
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+const hpaFilePath = process.env.HPA_FILE_PATH;
 
 app.post("/update-hpa", (req, res) => {
-  const { averageValue, averageUtilization } = req.body;
+  let { averageValue, averageUtilization } = req.body;
 
-  // Read the existing HPA configuration file
-  fs.readFile("hpa.yaml", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Error reading HPA configuration file");
-    }
+  if (!averageValue || isNaN(parseFloat(averageValue))) {
+    averageValue = "500";
+  }
 
-    let hpaConfig = yaml.parse(data);
+  if (!averageUtilization || isNaN(parseInt(averageUtilization))) {
+    averageUtilization = 50;
+  }
 
-    // Update the values
-    hpaConfig.spec.metrics[0].pods.target.averageValue =
-      parseInt(averageValue) + "m";
-    hpaConfig.spec.metrics[1].resource.target.averageUtilization =
-      parseInt(averageUtilization);
-    // Convert the object back to YAML
-    const newYaml = yaml.stringify(hpaConfig);
+  const insertQuery =
+    "INSERT INTO hpa_config (average_http_value, average_utilisation_percentage) VALUES ($1, $2)";
 
-    // Write the updated YAML back to the file
-    fs.writeFile("hpa.yaml", newYaml, (err) => {
+  pool.query(
+    insertQuery,
+    [parseInt(averageValue), parseInt(averageUtilization)],
+    (err, result) => {
       if (err) {
-        console.error(err);
-        return res.status(500).send("Error writing HPA configuration file");
+        console.error("Error inserting data into PostgreSQL:", err);
+        return res.status(500).send("Error inserting data into PostgreSQL");
       }
 
-      // Here you can also apply the updated configuration to the cluster
-      // using a Kubernetes client library or `kubectl apply -f hpa.yaml`
-      console.log(`Average Value: ${averageValue} Average Utilization: ${averageUtilization}`);
-      res.send("HPA configuration updated successfully");
-    });
-  });
+      console.log("Data inserted into PostgreSQL:", result.rows);
+
+      fs.readFile(hpaFilePath, "utf8", (readErr, data) => {
+        if (readErr) {
+          console.error("Error reading HPA configuration file:", readErr);
+          return res.status(500).send("Error reading HPA configuration file");
+        }
+
+        let hpaConfig;
+        try {
+          hpaConfig = yaml.parse(data);
+        } catch (parseErr) {
+          console.error("Error parsing HPA configuration file:", parseErr);
+          return res.status(500).send("Error parsing HPA configuration file");
+        }
+
+        hpaConfig.spec.metrics[0].pods.target.averageValue = `${parseFloat(
+          averageValue
+        )}m`;
+        hpaConfig.spec.metrics[1].resource.target.averageUtilization =
+          parseInt(averageUtilization);
+
+        const newYaml = yaml.stringify(hpaConfig);
+
+        fs.writeFile(hpaFilePath, newYaml, (writeErr) => {
+          if (writeErr) {
+            console.error("Error writing HPA configuration file:", writeErr);
+            return res.status(500).send("Error writing HPA configuration file");
+          }
+          console.log("kubectl apply -f ", hpaFilePath);
+          exec(`kubectl apply -f ${hpaFilePath}`, (execErr, stdout, stderr) => {
+            if (execErr) {
+              console.error(`Error applying configuration: ${stderr}`);
+              return res.status(500).send("Error applying HPA configuration");
+            }
+
+            console.log(`kubectl apply stdout: ${stdout}`);
+            return res.send(
+              "HPA configuration updated and applied successfully"
+            );
+          });
+        });
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,52 @@
-import  { useState } from 'react';
-import axios from 'axios';
-import Navbar from './Navbar';
+import { useState } from "react";
+import axios from "axios";
+import Navbar from "./Navbar";
 
 function App() {
-  const [averageValue, setAverageValue] = useState('');
-  const [averageUtilization, setAverageUtilization] = useState('');
-  const [result, setResult] = useState('');
-
+  const [averageValue, setAverageValue] = useState("");
+  const [averageUtilization, setAverageUtilization] = useState("");
+  const [result, setResult] = useState("");
 
   const submitCustomValues = async () => {
     try {
-      const response = await axios.post('http://localhost:3000/update-hpa', { averageValue: parseInt(averageValue), averageUtilization: parseInt(averageUtilization)});
+      const response = await axios.post("http://localhost:3001/update-hpa", {
+        averageValue: parseInt(averageValue),
+        averageUtilization: parseInt(averageUtilization),
+      });
       setResult(response.data);
     } catch (error) {
-      console.error('Error fetching custom metrics:', error);
-      setResult('Error fetching custom metrics');
+      console.error("Error fetching custom metrics:", error);
+      setResult("Error fetching custom metrics");
     }
   };
 
   return (
     <div className="App flex flex-col justify-center items-center gap-4 space-y-4">
       <Navbar />
-      <h1 className='text-5xl pt-12 font-bold'>Enter Custom Metrics</h1>
-      <input 
-        type="number" 
-        value={averageValue} 
-        onChange={(e) => setAverageValue(e.target.value)} 
+      <h1 className="text-5xl pt-12 font-bold">Enter Custom Metrics</h1>
+      <input
+        type="number"
+        value={averageValue}
+        onChange={(e) => setAverageValue(e.target.value)}
         placeholder="Enter the average value"
-        className='border border-gray-300 p-2 rounded-md outline-none'
+        className="border border-gray-300 p-2 rounded-md outline-none"
       />
-
-      <input 
-        type="number" 
-        value={averageUtilization} 
-        onChange={(e) => setAverageUtilization(e.target.value)} 
+      <input
+        type="number"
+        value={averageUtilization}
+        onChange={(e) => setAverageUtilization(e.target.value)}
         placeholder="Enter the average utilization"
-        className='border border-gray-300 p-2 rounded-md outline-none'
+        className="border border-gray-300 p-2 rounded-md outline-none"
       />
-      <button onClick={submitCustomValues} className='bg-[#01a982] font-bold'>Enter</button>
-      <p className='font-semibold'>{result}</p>
-      <hr className='w-full' />
-      <h2 className='text-5xl font-bold'>Results</h2>
+      <button onClick={submitCustomValues} className="bg-[#01a982] font-bold">
+        Enter
+      </button>
+      <p className="font-semibold">{result}</p>
+      <hr className="w-full" />
+      <h2 className="text-5xl font-bold">Results</h2>
       {/* Results will be displayed here */}
       {/* <pre className='border-[1.6px] rounded-xl p-6 font-semibold text-xl'>Total HTTP Requests {metrics}</pre> */}
-      <button className='bg-[#01a982] font-bold'>Refresh Metrics</button>
+      <button className="bg-[#01a982] font-bold">Refresh Metrics</button>
     </div>
   );
 }
